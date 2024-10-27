fadda23b366a50181f49ddacac2c23d9ece8b1ae
Ben <126756817+btmarsh6@users.noreply.github.com>
2024-10-26 18:15:10 -0700

    changed openai captioner to take image

--- modules/openai_story_teller.py
+++ modules/openai_story_teller.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 import os
+import base64
 
 
 class OpenAIStoryTeller:
@@ -12,7 +13,7 @@ class OpenAIStoryTeller:
         """
         self.lang = lang
 
-    def generate_story(self, text_from_visuals, extracted_text):
+    def generate_story(self, image_path, extracted_text):
         """
         Generates a LLM optimized story from the provided text.
 
@@ -30,29 +31,37 @@ class OpenAIStoryTeller:
             client = OpenAI(
                 api_key=api_key,  # this is also the default, it can be omitted
             )
-
-            response = client.chat.completions.with_raw_response.create(
-                messages=[
-                    {
-                        "role": "system",
-                        "content": """Imagine you are telling a story from one page of a children’s storybook page. Summarize the description in simple, engaging language as if you are narrating a story aloud, using phrases and words that bring each scene to life and create a warm, friendly tone. Avoid adding any new information that is not in the text of the page. Keep it under 50 words
-                        """,
-                    },
-                    {
+            with open(image_path, "rb") as image_file:
+                b64_image = base64.b64encode(image_file.read()).decode("utf-8")
+                response = client.chat.completions.create(
+                    messages=[
+                        {
                         "role": "user",
-                        "content": f"Here is the page description of the visuals: {text_from_visuals}. More importantly, here is a text from the page in the page: {extracted_text}.",
-                    },
-                ],
-                model="gpt-4o-mini",
-                temperature=0.3,  # Adjust the temperature to control the creativity of the response
-            )
-            print(response.headers.get("x-ratelimit-limit-tokens"))
-
-            # get the object that `chat.completions.create()` would have returned
-            completion = response.parse()
-            first_choice = completion.choices[0].message.content
-
-            return first_choice
+                        "content": [
+                            {
+                            "type": "text",
+                            "text": """You are a children's storybook author. You are given part of the story text and the picture from that page.
+            Write a descriptive caption for the picture, so a small child who cannot see the image can understand what is happening.
+            """,
+                            },
+                            {
+                            "type": "text",
+                            "text": extracted_text,
+                            },
+                            {
+                            "type": "image_url",
+                            "image_url": {
+                                "url":  f"data:image/jpeg;base64,{b64_image}"
+                            },
+                            },
+                        ],
+                        }
+                    ],
+                    model="gpt-4o-mini",
+                    temperature=0.3,  # Adjust the temperature to control the creativity of the response
+                )
+            return response.choices[0].message.content
+        
         except Exception as e:
             print(f"Error generating llm optimized story: {e}")
             return ""
